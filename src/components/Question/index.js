d3d9bf17afcd68e34512c7035f9b9a7887a8a17a
SirYummy <micahbole.n@gmail.com>
2018-03-08 15:54:28 -0600

    chore: add propTypes for Question component

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,3 +1,11 @@
-export default (props) => {
+import PropTypes from 'prop-types'
+
+const Question (props) => {
     return (props.questionRenderer)
 }
+
+Question.propTypes = {
+    questionRenderer : PropTypes.element.isRequired
+}
+
+export default Question
